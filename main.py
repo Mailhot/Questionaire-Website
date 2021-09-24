4279b0da409db1d68f1aa205ae96f7c7279a52eb
Mailhot <36643298+Mailhot@users.noreply.github.com>
2021-09-24 16:12:55 -0400

    Update main.py
    added app.models

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from app import app, db
-from models import User, Question, QuestionChoice, UserAnswer
+from app.models import User, Question, QuestionChoice, UserAnswer
 
 
 @app.shell_context_processor
