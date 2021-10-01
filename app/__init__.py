98524db603f14325eeeaa7faaa80512038a894b6
Mailhot <36643298+Mailhot@users.noreply.github.com>
2021-10-01 14:59:15 -0400

    trying again

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_admin import Admin
-import dbDefault as db_init
+from app import dbDefault as db_init
 
 app = Flask(__name__)
 app.config.from_object(Config)
