7df4869aa19a84f5356d7158cf656b96aaf6a661
Mailhot <36643298+Mailhot@users.noreply.github.com>
2021-10-01 16:11:05 -0400

    again

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 app.config.from_object(Config)
 app.static_folder = 'static'
 db = SQLAlchemy(app)
-from app import models as models_init
+from app.models import models as models_init
 models_init.db_init()
 migrate = Migrate(app, db)
 login = LoginManager(app)
