483f0d5439b0dc2391f33d0bb9f5f11817d6e19d
Mailhot <36643298+Mailhot@users.noreply.github.com>
2021-10-01 13:45:47 -0400

    updated database uri to work with gcloud db

--- config.py
+++ config.py
@@ -2,8 +2,18 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+USER = ""
+PASSWORD = ""
+DATABASE = ""
+CONNECTION_NAME = ""
+
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    #     'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = (
+            'mysql+pymysql://{user}:{password}@localhost/{database}'
+            '?unix_socket=/cloudsql/{connection_name}').format(
+                user=USER, password=PASSWORD,
+                database=DATABASE, connection_name=CONNECTION_NAME)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
