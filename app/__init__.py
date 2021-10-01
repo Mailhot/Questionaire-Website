7f3bfb8f08a479d65c28aa918762833c30940f93
Mailhot <36643298+Mailhot@users.noreply.github.com>
2021-10-01 14:53:45 -0400

    Added the init db for test (this will need to be removed)

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,13 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_admin import Admin
+import dbDefault as db_init
 
 app = Flask(__name__)
 app.config.from_object(Config)
 app.static_folder = 'static'
 db = SQLAlchemy(app)
+db_init
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = "login"
