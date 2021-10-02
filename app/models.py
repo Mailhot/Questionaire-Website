e6a222fe89f665b2bfaa76894216223fb053e6ea
Mailhot <36643298+Mailhot@users.noreply.github.com>
2021-10-02 08:41:07 -0400

    Update models.py
    corrected exception

--- app/models.py
+++ app/models.py
@@ -137,29 +137,29 @@ def create_quiz(*args, **kwargs):
     # Get admin as user
     try:
         admin1 = db.session.query(User).filter(name='admin').one()  # filter on name
-    except MultipleResultsFound, e:
+    except MultipleResultsFound as e:
         print e
         # Deal with it
-    except NoResultFound, e:
+    except NoResultFound as e:
         print e
         # Deal with that as well
 
     # get quiz style
     try:
         quizstyle1 = db.session.query(QuizStyle).filter(style_name='Old flag style').one()  # filter on name
-    except MultipleResultsFound, e:
+    except MultipleResultsFound as e:
         print e
         # Deal with it
-    except NoResultFound, e:
+    except NoResultFound as e:
         print e
         # Deal with that as well
 
     try:
         quizstyle2 = db.session.query(QuizStyle).filter(style_name='Language quiz style').one()  # filter on name
-    except MultipleResultsFound, e:
+    except MultipleResultsFound as e:
         print e
         # Deal with it
-    except NoResultFound, e:
+    except NoResultFound as e:
         print e
         # Deal with that as well
 
@@ -187,20 +187,20 @@ def create_quizcontent(*args, **kwargs):
     # get te quiz
     try:
         quiz1 = db.session.query(Quiz).filter(quizname='Flag Quiz').one()  # filter on name
-    except MultipleResultsFound, e:
+    except MultipleResultsFound as e:
         print e
         # Deal with it
-    except NoResultFound, e:
+    except NoResultFound as e:
         print e
         # Deal with that as well
     db.session.add(QuizContent(quiz_id=qiz1.id,text_content="Are you truly aware of the outside world? Do you have what it takes to test your knowledge on the flags of the world? Take our test !", img_content="au.svg"))
 
     try:
         quiz2 = db.session.query(Quiz).filter(quizname='Language Quiz').one()  # filter on name
-    except MultipleResultsFound, e:
+    except MultipleResultsFound as e:
         print e
         # Deal with it
-    except NoResultFound, e:
+    except NoResultFound as e:
         print e
         # Deal with that as well
     db.session.add(QuizContent(quiz_id=quiz2.id,text_content="How much do you know about world culture, information and languages?",img_content="people-banner.png"))
@@ -236,19 +236,19 @@ class Question(db.Model):
 def create_question(*args, **kwargs):
     try:
         quiz1 = db.session.query(Quiz).filter(quizname='Flag Quiz').one()  # filter on name
-    except MultipleResultsFound, e:
+    except MultipleResultsFound as e:
         print e
         # Deal with it
-    except NoResultFound, e:
+    except NoResultFound as e:
         print e
         # Deal with that as well
 
     try:
         quiz2 = db.session.query(Quiz).filter(quizname='Language Quiz').one()  # filter on name
-    except MultipleResultsFound, e:
+    except MultipleResultsFound as e:
         print e
         # Deal with it
-    except NoResultFound, e:
+    except NoResultFound as e:
         print e
         # Deal with that as well
 
